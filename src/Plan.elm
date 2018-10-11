09f20eec3f6f374721a50b96d031ceda3b023af6
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-10-11 06:57:49 +0200

    Provide a definition for the ProblemDefinition

--- src/Plan.elm
+++ src/Plan.elm
@@ -3,15 +3,21 @@ module Plan exposing (Strategy)
 {-| This module provides a machinery to solve a planning problem.
 -}
 
+import SchoolOfUnderstanding as School
+
 
 {-| A `Strategy` describes a way to solve a planning problem.
 -}
 type alias Strategy =
     ProblemDefinition -> Stream Candidate
 
-
+{-| Definition of a specific planning problem. -}
 type alias ProblemDefinition =
-    {}
+    { availableSlots : School.Slots
+    , groupsToTeach : List School.Group
+    , availableTeachers : List School.Teacher
+    , participatingStudents : List School.Student
+    }
 
 
 type alias Stream a =

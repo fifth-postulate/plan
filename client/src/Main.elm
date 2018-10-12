bf9406c1d0b7ff1001b02a1232fb2edf460493e8
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-10-12 11:11:26 +0200

    Create an Stream Int visualization

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -1,9 +1,60 @@
 module Main exposing (main)
 
-import Html
+import Browser
+import Html exposing (Html)
+import Html.Events as Event
 import Plan
 import SchoolOfUnderstanding
+import Stream exposing (Stream)
 
 
 main =
-    Html.text "Hello, World!"
+    Browser.sandbox
+        { init = init <| List.range 1 10
+        , update = update
+        , view = view viewInt
+        }
+
+
+init : List a -> Model a
+init list =
+    { stream = Stream.fromList list }
+
+
+type alias Model a =
+    { stream : Stream a
+    }
+
+
+type Message
+    = Advance
+    | Retrograde
+
+
+update : Message -> Model a -> Model a
+update message model =
+    case message of
+        Advance ->
+            { model | stream = Stream.advance model.stream }
+
+        Retrograde ->
+            { model | stream = Stream.retrograde model.stream }
+
+
+view : (Maybe a -> Html Message) -> Model a -> Html Message
+view elementView model =
+    Html.div []
+        [ Html.button [Event.onClick Retrograde] [ Html.text "←" ]
+        , elementView (Stream.peek model.stream)
+        , Html.button [Event.onClick Advance] [ Html.text "→" ]
+        ]
+
+
+viewInt : Maybe Int -> Html Message
+viewInt value =
+    case value of
+        Just n ->
+            Html.span [] [ Html.text <| String.fromInt n ]
+
+        Nothing ->
+            Html.span [] [ Html.text <| "-" ]

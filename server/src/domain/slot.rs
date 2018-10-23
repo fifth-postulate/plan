2a38c39bf382528caf8085d4640b811ca63dd9d4
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-10-23 06:59:19 +0200

    Test deserialization for Slot and TimeOfDay

--- server/src/domain/slot.rs
+++ server/src/domain/slot.rs
@@ -12,16 +12,53 @@ pub enum Weekday {
     Sunday,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct Slot {
     start: TimeOfDay,
     finish: TimeOfDay,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+impl Slot {
+    pub fn new(start: TimeOfDay, finish: TimeOfDay) -> Slot {
+        Slot {start, finish}
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct TimeOfDay {
     hour: u8,
     minutes: u8,
 }
 
+impl TimeOfDay {
+    pub fn new(hour: u8, minutes: u8) -> TimeOfDay {
+        TimeOfDay {hour, minutes}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate serde;
+    extern crate serde_json;
+
+    use super::*;
+
+    #[test]
+    fn serialize_time_of_day() {
+        let data = "{\"hour\": 8, \"minutes\": 30}";
+        let actual: TimeOfDay = serde_json::from_str(&data).unwrap();
+        let expected = TimeOfDay::new(8, 30);
+
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn serialize_slot() {
+        let data = "{\"start\":{\"hour\": 8,\"minutes\": 30},\"finish\": {\"hour\": 9,\"minutes\": 15}}";
+        let actual: Slot = serde_json::from_str(&data).unwrap();
+        let expected = Slot::new(TimeOfDay::new(8, 30), TimeOfDay::new(9, 15));
+
+        assert_eq!(actual, expected)
+    }
+}
 

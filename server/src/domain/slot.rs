41559471f077c29622202e9d227427b488828bc9
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-10-23 06:45:38 +0200

    Derive Serializer and Deserializers for slot

--- server/src/domain/slot.rs
+++ server/src/domain/slot.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 
 pub type Slots = HashMap<Weekday, Vec<Slot>>;
 
+#[derive(Serialize, Deserialize, Debug)]
 pub enum Weekday {
     Monday,
     Tuesday,
@@ -11,11 +12,13 @@ pub enum Weekday {
     Sunday,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Slot {
     start: TimeOfDay,
     finish: TimeOfDay,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 pub struct TimeOfDay {
     hour: u8,
     minutes: u8,

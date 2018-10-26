8fc9c8bcdfef381d1a9223c917c4e16677c04d03
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-10-26 18:34:45 +0200

    Create a websocket to the server

--- server/static/js/app.js
+++ server/static/js/app.js
@@ -2,4 +2,10 @@
     var app = Elm.Main.init({
         node: document.getElementById('plan')
     });
+
+    // Until elm/websocket is released.
+    var socket = new WebSocket('ws://localhost:8326');
+    socket.addEventListener('message', function(event){
+        console.log(event.data);
+    });
 })();
